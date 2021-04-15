7a0f98d73f6168c7a4c8be231a138af6b3ef8ccf
patrick hrabos <phrabos@gmail.com>
2021-04-15 13:03:23 -0400

    add users table and controller

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,15 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
 
 CREATE TABLE users (
     github_username TEXT NOT NULL PRIMARY KEY,
     github_photo_url TEXT NOT NULL
 );
+
+CREATE TABLE posts (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user TEXT NOT NULL REFERENCES users(github_username),
+    photo_url TEXT NOT NULL,
+    caption TEXT NOT NULL,
+    tags TEXT[]
+)
