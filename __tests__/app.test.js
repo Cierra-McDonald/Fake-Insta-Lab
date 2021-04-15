40b3800ae126bddbb857766ce5b04def5fa276a5
patrick hrabos <phrabos@gmail.com>
2021-04-15 19:42:45 -0400

    add seed data file for tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const User = require('../lib/models/User');
+const Posts = require('../lib/models/Posts');
+const seed = require('../lib/utils/testData');
 
 
 
@@ -10,18 +12,19 @@ describe('lab-13-fake-instagram routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  beforeEach(() => { return seed(); }); 
 
   it('adds a post to the database', async ()=>{
 
-    const newUser = await User.insert({
-      username: 'patrick',
-      photoUrl: 'http://avatar.com'
-    })
+    // const newUser = await User.insert({
+    //   username: 'test_user',
+    //   photoUrl: 'http://avatar.com'
+    // })
 
     const res = await request(app)
     .post('/api/v1/posts')
     .send({
-      user: newUser.username,
+      user: 'test_user',
       photo_url: 'http://photo.com',
       caption: 'coolbeans',
       tags: null
@@ -29,10 +32,44 @@ describe('lab-13-fake-instagram routes', () => {
     })
     expect(res.body).toEqual({
       id: expect.any(String),
-      user: newUser.username,
+      user: 'test_user',
       photoUrl: 'http://photo.com',
       caption: 'coolbeans',
       tags: null
     })
+  })
+  it('gets all posts from db', async()=>{
+    // const newUser = await User.insert({
+    //   username: 'test_user',
+    //   photoUrl: 'http://avatar.com'
+    // })
+
+    // await Posts.create('test_user','http://photo.com','coolbeans',
+    // )
+    // await Posts.create('test_user','http://photo.com','test post',
+    // )
+    const res = await request(app)
+    .get('/api/v1/posts')
+
+    expect(res.body).toEqual([{
+      id: expect.any(String),
+      user: 'test_user',
+      photoUrl: 'http://photo.com',
+      caption: 'coolbeans',
+      tags: null
+
+    },
+    {
+      id: expect.any(String),
+      user: 'test_user',
+      photoUrl: 'http://photo.com',
+      caption: 'test post',
+      tags: null
+
+    }])
+  })
+  it('gets a post by id', async ()=>{
+    const res = await request(app)
+    .get('/api/v1/posts/1')
   })
 });
